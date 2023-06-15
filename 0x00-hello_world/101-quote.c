770d8d89758a8634713a41b87be82c629d0b93c9
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-06-15 13:37:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - a C program that prints a new line to the standard error
  * Return 1 (Success)
  */
-int main(void)
+int main()
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 57);
+return 1;
 }
