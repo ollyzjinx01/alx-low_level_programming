467aac0780dd82c1202e175d0a935fbe7137a24c
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-06-15 13:45:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
  * main - a C program that prints a new line to the standard error
  * Return 1 (Success)
  */
-int main()
+int main(void)
 {
-char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 57);
-return 1;
+char *messge = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, messge, 57);
+return (1);
 }
