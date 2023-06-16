d75a8d80829732b1316a081308e680e7af1bb829
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-06-16 14:53:02 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all possible different combinations
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int a = '0';
+int b = '0';
+while (a <= '9')
+{
+while (b <= '9')
+{
+if (!(a > b) || a == b)
+{
+putchar(a);
+putchar(b);
+if (a == '8' && b == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+b++;
+}
+b = '0';
+a++;
+}
+return (0);
+}
