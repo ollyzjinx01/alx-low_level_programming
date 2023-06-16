a7c23c966034595dbd31c166e4fecda7d6f86ca5
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-06-16 15:41:14 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,16 @@ for (j = i + '1'; j <= '9'; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i != '8' || j != '9')
+if (i == '8' && j == '9')
+{
+putchar('\n');
+}
+else
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
-putchar('\n');
 return (0);
 }
