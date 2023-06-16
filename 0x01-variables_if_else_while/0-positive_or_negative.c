e8860c3a495aecdb189859bcf7ddb759bdc120f8
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-06-16 11:58:28 -0700

    0-positive_or_negative clear vi] vi"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 	printf("%d is positive\n", n);
 else if (n == 0)
-	printif("%d is zero\n", n);
+	printf("%d is zero\n", n);
 else
-	printf("%d is negative\n", n);
-	return (0);
+printf("%d is negative\n", n);
+return (0);
 }
