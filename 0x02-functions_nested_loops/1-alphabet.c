dd64692c820feff912a066c063ed3883cc408071
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-06-20 13:35:53 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - lower case alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char sh;
+for (sh = 'a'; sh <= 'z'; sh++)
+_putchar(sh);
+_putchar('\n');
+}
