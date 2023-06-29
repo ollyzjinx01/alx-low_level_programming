1f9685a76be72f564297a8b2ce94f740139571b7
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-06-29 16:31:14 -0700

    103-add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,42 +1,79 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
+
 /**
- * *infinite_add - a function that adds two numbers
- * @n1: number to add
- * @n2: numer to add
- * @size_r: ....
- * @r: .....
- * Return: Nothingg
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = strlen(n1);
-int len2 = strlen(n2);
-int carry = 0;
-if (len1 + len2 >= size_r)
-return (0);
-int i = len1 - 1;
-int j = len2 - 1;
-int k = 0;
-while ((i >= 0) || (j >= 0) || (carry > 0))
-{
-int digit1 = (i >= 0) ? n1[i] - '0' : 0;
-int digit2 = (j >= 0) ? n2[j] - '0' : 0;
-int sum = digit1 + digit2 + carry;
-carry = sum / 10;
-int digit = sum % 10;
-r[k] = digit + '0';
-i--;
-j--;
-k++;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
-for (int l = 0; l < k / 2; l++)
-{
-char temp = r[l];
-r[l] = r[k - 1 - l];
-r[k - 1 - l] = temp;
-}
-r[k] = '\0';
-return (r);
-}
+
