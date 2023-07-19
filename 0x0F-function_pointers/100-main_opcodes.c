1d2bc19d66d72930589d3d5df4975ecd05df7e3f
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-07-19 14:01:02 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: Always 0 (Success)
- */
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  * @print_opcodes: ...
+  * @num_bytes: ...
+  * Return: ...
+  */
+void print_opcodes(int num_bytes)
+{
+	unsigned char *ptr = (unsigned char *)print_opcodes;
+	int i;
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", ptr[i]);
+	}
+	printf("\n");
+}
 int main(int argc, char *argv[])
 {
-		int bytes, i;
-		char *arr;
-		if (argc != 2)
-		{
-			printf("Error\n");
-			exit(1);
-		}
-		bytes = atoi(argv[1]);
-		if (bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-		arr = (char *)main;
-		for (i = 0; i < bytes; i++)
-		{
-			if (i == bytes - 1)
-			{
-				printf("%02hhx\n", arr[i]);
-				break;
-			}
-			printf("%02hhx ", arr[i]);
-		}
-		return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return 1;
 	}
+	int num_bytes = atoi(argv[1]);
+	if (num_bytes <= 0)
+	{
+		printf("Error\n");
+		return 2;
+	}
+	print_opcodes(num_bytes);
+	return 0;
+}
