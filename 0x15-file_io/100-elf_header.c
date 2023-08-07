fc0dcb26872d8d93ce7b2d9bb53d3ecc72ca4dc1
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-08-07 15:11:23 -0700

    100-elf_header.c updated again

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,82 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /**
- * display_elf_header - ......
- * @filename: magic.
+ * read_elf_header - ......
+ * @fd: .....
+ * @header: .....
  * Return: no return.
  */
 
-void display_elf_header(const char *filename)
+void read_elf_header(int fd, Elf64_Ehdr *header)
 {
-	int fd;
-	Elf64_Ehdr elf_header;
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
 	{
-		fprintf(stderr, "Error opening file: %s\n", filename);
+		perror("read");
 		exit(98);
 	}
-	
-	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-	{
-		fprintf(stderr, "Error reading ELF header from file: %s\n", filename);
-		close(fd);
-		exit(98);
-	}
-	
-	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-		elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-		elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-		elf_header.e_ident[EI_MAG3] != ELFMAG3)
-		{
-		fprintf(stderr, "File %s is not an ELF file.\n", filename);
-		close(fd);
-		exit(98);
-	}
-
-	printf("Magic: %c%c%c%c\n",
-		   elf_header.e_ident[EI_MAG0], elf_header.e_ident[EI_MAG1],
-		   elf_header.e_ident[EI_MAG2], elf_header.e_ident[EI_MAG3]);
-
-	printf("Class: %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-
-	printf("Data: %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-
-	printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
-
-	printf("OS/ABI: %s\n", elf_header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Unknown");
-
-	printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
-
-	printf("Type: %s\n", elf_header.e_type == ET_EXEC ? "EXEC (Executable file)" : "Unknown");
-
-	printf("Entry point address: 0x%lx\n", (unsigned long)elf_header.e_entry);
-
-	close(fd);
 }
+
 /**
- * main - address
- * @argc: .....
- * @argv: .....
+ * validate_elf_file - ........
+ * @header: .......
+ * Return: no return.
+ */
+
+void validate_elf_file(Elf64_Ehdr *header)
+{
+	if (header->e_ident[EI_MAG0] != ELFMAG0 ||
+	    header->e_ident[EI_MAG1] != ELFMAG1 ||
+	    header->e_ident[EI_MAG2] != ELFMAG2 ||
+	    header->e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "Error: not an ELF file\n");
+		exit(98);
+	}
+}
+
+/**
+ * display_elf_header - ........
+ * @header: ......
+ * Return: no return.
+ */
+
+void display_elf_header(Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\nClass:                             ");
+	printf("%s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("Data:                              ");
+	printf("%s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's, l" : "2's, b");
+	printf("Version:                           ");
+	printf("%u\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI:                            ");
+	printf("%u\n", header->e_ident[EI_OSABI]);
+	printf("ABI Version:                       ");
+	printf("%u\n", header->e_ident[EI_ABIVERSION]);
+	printf("Type:                              ");
+	printf("%u\n", header->e_type);
+	printf("Entry point address:               ");
+	printf("%#010x\n", (unsigned int)header->e_entry);
+}
+
+/**
+ * main - ......
+ * @argc: ....
+ * @argv: ....
  * Return: no return.
  */
 
 int main(int argc, char *argv[])
 {
+	int fd;
+	Elf64_Ehdr header;
+
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-		exit(98);
+		fprintf(stderr, "Usage: elf_header elf_filename\n");
+		return (98);
 	}
+	fd = open(argv[1], O_RDONLY);
 
-	display_elf_header(argv[1]);
+	if (fd < 0)
+	{
+		perror("open");
+		return (98);
+	}
+	read_elf_header(fd, &header);
 
-	return 0;
+	validate_elf_file(&header);
+	display_elf_header(&header);
+
+	close(fd);
+	return (0);
 }
+
